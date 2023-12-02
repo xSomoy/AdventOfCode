d2fde5b699c3fc1cf909c899f37f1bb7842f07a1
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-12-02 21:26:18 +0600

    day1_p2_progress

--- 2023/Day1_Trebuchet/solution_part2.py
+++ 2023/Day1_Trebuchet/solution_part2.py
@@ -6,18 +6,32 @@ Lines = input_file.readlines()
 
 def extract_numbers(text):
     # Define a dictionary to store the mapping of words to numbers
-    number_mapping = ['zero', 'one', 'two', 'three',
-                      'four', 'five', 'six', 'seven', 'eight', 'nine',
-                      '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    string_number_mapping = {
+        'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4,
+        'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9
+        # Add more mappings as needed
+    }
+    numbers = ['zero', 'one', 'two', 'three',
+               'four', 'five', 'six', 'seven', 'eight', 'nine',
+               '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
     # Use regular expression to find words representing numbers in the text
-    value = []
-    for i in number_mapping:
+    index = []
+    values = []
+    for i in numbers:
         if i in text:
-            value.append(text.index(i))
-
-        # print(i)
-    return value
+            index.append(text.index(i))
+            values.append(i)
+    unconverted_vals = [values[index.index(min(index))],
+                        values[index.index(max(index))]]
+    converted_vals = []
+    for i in unconverted_vals:
+        if len(i) > 1:
+            converted_vals.append(string_number_mapping[i])
+        else:
+            converted_vals.append(int(i))
+    final_val = int(str(converted_vals[0]) + str(converted_vals[1]))
+    return final_val
 
 
 print(extract_numbers('two2seven7'))
