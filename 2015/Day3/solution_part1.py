6120242838f26788859535807543c32186efd15e
John Deadman <47719599+xSomoy@users.noreply.github.com>
2023-12-03 04:47:34 +0600

    2015day3_p1_done

--- 2015/Day3/solution_part1.py
+++ 2015/Day3/solution_part1.py
@@ -1,2 +1,18 @@
-input_file = open('input.txt', 'r')
-Lines = input_file.readlines()
+input_file = open('input.in', 'r')
+Lines = input_file.read().strip()
+
+visited = set()
+visited.add((0, 0))
+pos = (0, 0)
+
+for i in Lines:
+    if i == '^':
+        pos = (pos[0], pos[1] + 1)
+    elif i == 'v':
+        pos = (pos[0], pos[1] - 1)
+    elif i == '>':
+        pos = (pos[0] + 1, pos[1])
+    if i == '<':
+        pos = (pos[0] - 1, pos[1])
+    visited.add(pos)
+print(len(visited))
