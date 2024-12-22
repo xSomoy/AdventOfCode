e0752b8533b9d52014511c16f5dfa45bf6791eaf
TENET <xSomoy@protonmail.com>
2024-12-23 01:35:30 +0600

    Day 1 working

--- 2024/Day1/solution_part1.py
+++ 2024/Day1/solution_part1.py
@@ -7,10 +7,13 @@ right_col = []
 for i in Lines:
     left_col.append(i.split()[0])
 left_col = sorted(left_col)
-for i in left_col:
-    print(i)
 
 
 for i in Lines:
     right_col.append(i.split()[1])
 right_col = sorted(right_col)
+
+i = 0
+while (0 < len(left_col)):
+    print(int(left_col[i]) - int(right_col[i]))
+    i += 1
