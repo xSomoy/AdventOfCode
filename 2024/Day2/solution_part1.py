5ffa94dc88bcecdb1d688827a6e1c6ee44cce1b2
TENET <xSomoy@protonmail.com>
2024-12-25 16:48:21 +0600

    working on d2p1

--- 2024/Day2/solution_part1.py
+++ 2024/Day2/solution_part1.py
@@ -1,16 +1,19 @@
 input_file = open('demo_input.txt', 'r')
 levels = input_file.readlines()
 current_level = []
-status = ""
+status = 1
 for i in levels:
-    j = 0
+    j = 1
     current_level = i
     current_level = current_level.split()
-    print(len(current_level))
-    while j < len(current_level)+1:
-        if (abs(int(current_level[j]) - int(current_level[j+1])) <= 2):
-            status = "safe"
+    while j < len(current_level):
+        if (int(current_level[j-1]) < int(current_level[j])):
+            status += 1
         else:
-            status = "unsafe"
+            status -= 1
         j += 1
-print(status)
+
+    if status < 4:
+        print("Not Increasing")
+    else:
+        print("Increasing")
