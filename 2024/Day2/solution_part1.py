596a34337fde4b54f005f67ee2978b52bc051d17
TENET <xSomoy@protonmail.com>
2024-12-25 15:53:08 +0600

    working on d2p1

--- 2024/Day2/solution_part1.py
+++ 2024/Day2/solution_part1.py
@@ -3,13 +3,14 @@ levels = input_file.readlines()
 current_level = []
 status = ""
 for i in levels:
-    current_level = i
-
-for i in current_level:
     j = 0
-    k = 1
-    data1 = current_level[0]
-    data1 = int(data1)
-    data2 = current_level[1]
-    data2 = int(data2)
-    print(type(data2))
+    current_level = i
+    current_level = current_level.split()
+    print(len(current_level))
+    while j < len(current_level)+1:
+        if (abs(int(current_level[j]) - int(current_level[j+1])) <= 2):
+            status = "safe"
+        else:
+            status = "unsafe"
+        j += 1
+print(status)
