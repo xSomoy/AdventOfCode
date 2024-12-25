51952f24efdceb73a0581a55e3643ac4a5ff191c
TENET <xSomoy@protonmail.com>
2024-12-25 17:27:40 +0600

    working on d2p1

--- 2024/Day2/solution_part1.py
+++ 2024/Day2/solution_part1.py
@@ -1,19 +1,16 @@
 input_file = open('demo_input.txt', 'r')
 levels = input_file.readlines()
 current_level = []
-status = 1
+
 for i in levels:
     j = 1
+    status = 0
     current_level = i
     current_level = current_level.split()
     while j < len(current_level):
-        if (int(current_level[j-1]) < int(current_level[j])):
-            status += 1
+        if (abs(int(current_level[j-1]) - int(current_level[j]))) <= 3:
+            status = 1
         else:
-            status -= 1
+            status = 0
         j += 1
-
-    if status < 4:
-        print("Not Increasing")
-    else:
-        print("Increasing")
+    print(status)
