40ee4953247161f3efa41adb235ffd4b9e5d631e
TENET <xSomoy@protonmail.com>
2025-01-06 17:52:16 +0600

    2015 revisit

--- 2015/Day3/solution_part2 copy.py
+++ 2015/Day3/solution_part2 copy.py
@@ -15,7 +15,20 @@ for i in instruction:
             pos = (pos[0] + 1, pos[1])
         elif i == '<':
             pos = (pos[0] - 1, pos[1])
-    visited.add(pos)
+        visited.add(pos)
+        n += 1
+    elif n != 0:
+        if i == '^':
+            pos = (pos[0], pos[1] + 1)
+        elif i == 'v':
+            pos = (pos[0], pos[1] - 1)
+        elif i == '>':
+            pos = (pos[0] + 1, pos[1])
+        elif i == '<':
+            pos = (pos[0] - 1, pos[1])
+        visited.add(pos)
+        n -= 1
 
-print(visited)
+
+# print(visited)
 print(len(visited))
