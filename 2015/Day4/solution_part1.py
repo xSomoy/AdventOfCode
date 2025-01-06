6f99cd31d6292e56e41eeac171f094e9e8adaf45
TENET <xSomoy@protonmail.com>
2025-01-06 18:41:20 +0600

    d4p1 done

--- 2015/Day4/solution_part1.py
+++ 2015/Day4/solution_part1.py
@@ -5,5 +5,15 @@ def md5(key):
     return hashlib.md5(key.encode()).hexdigest()
 
 
-key = "yzbqklnj"
-print(md5(key))
+# seed = "abcdef609043"
+seed = "yzbqklnj"
+num = 0
+while True:
+    key = seed + str(num)
+    result = md5(key)
+    if result[0] == '0' and result[1] == '0' and result[2] == '0' and result[3] == '0' and result[4] == '0':
+        print("Hash starts with 5 zeros")
+        print(key + " md5 hash:  " + result)
+        print(num)
+        exit()
+    num += 1
