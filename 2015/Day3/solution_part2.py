1b8b9507fe10d2c6c7c8eda2b52d1a355601c218
TENET <xSomoy@protonmail.com>
2025-01-06 18:03:28 +0600

    2015 d3p2 solved

--- 2015/Day3/solution_part2.py
+++ 2015/Day3/solution_part2.py
@@ -1,15 +1,11 @@
-input_file = open('input.txt', 'r')
-Lines = input_file.read().strip()
-# Lines = '^v^v^v^v^v'
+instruction = open('input.txt', 'r').read().strip()
 
-santa_visited = set()
-robo_santa_visited = set()
-santa_visited.add((0, 0))
-robo_santa_visited.add((0, 0))
+visited = set()
+visited.add((0, 0))
 santa_pos = (0, 0)
 robo_santa_pos = (0, 0)
 n = 0
-for i in Lines:
+for i in instruction:
     if n == 0:
         if i == '^':
             santa_pos = (santa_pos[0], santa_pos[1] + 1)
@@ -19,7 +15,7 @@ for i in Lines:
             santa_pos = (santa_pos[0] + 1, santa_pos[1])
         elif i == '<':
             santa_pos = (santa_pos[0] - 1, santa_pos[1])
-        santa_visited.add(santa_pos)
+        visited.add(santa_pos)
         n += 1
     else:
         if i == '^':
@@ -30,7 +26,7 @@ for i in Lines:
             robo_santa_pos = (robo_santa_pos[0] + 1, robo_santa_pos[1])
         elif i == '<':
             robo_santa_pos = (robo_santa_pos[0] - 1, robo_santa_pos[1])
-        robo_santa_visited.add(robo_santa_pos)
+        visited.add(robo_santa_pos)
         n += -1
 
-print(len(santa_visited) + len(robo_santa_visited))
+print(len(visited))
