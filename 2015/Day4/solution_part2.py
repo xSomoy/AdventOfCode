2cbff0d6d234f1a81c0bf29eb707859f905c1fe2
TENET <xSomoy@protonmail.com>
2025-01-06 19:06:01 +0600

    d4p2 done

--- 2015/Day4/solution_part2.py
+++ 2015/Day4/solution_part2.py
@@ -11,8 +11,8 @@ num = 0
 while True:
     key = seed + str(num)
     result = md5(key)
-    if result[0] == '0' and result[1] == '0' and result[2] == '0' and result[3] == '0' and result[4] == '0':
-        print("Hash starts with 5 zeros")
+    if result[0] == '0' and result[1] == '0' and result[2] == '0' and result[3] == '0' and result[4] == '0' and result[5] == '0':
+        print("Hash starts with 6 zeros")
         print(key + " md5 hash:  " + result)
         print(num)
         exit()
