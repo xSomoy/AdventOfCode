82e9be7e14d7e1b5ef0d16851e37c734a6ef4fd6
TENET <xSomoy@protonmail.com>
2025-01-17 00:02:08 +0600

    d7p1 working

--- 2015/Day7/solution_part1.py
+++ 2015/Day7/solution_part1.py
@@ -1,12 +1,73 @@
 table = {}
+mem = {}
+roots = []
+step = {}
 lines = open('input.in', 'r').readlines()
 for i in lines:
     l, r = i.strip().split(" -> ")
     table[r] = l
+    step[i] = r
 
-# print(table['b'])
 
-t = bin(5)[2:].zfill(16)
-a = 10
-b = 4
-print(a >> 1)
+def findRoots():
+    for i in table:
+        if table[i].isdigit():
+            mem[i] = table[i]
+            roots.append(i)
+
+
+def ops(cmd):
+    if cmd.startswith("NOT"):
+        var = cmd.split()[1]
+        key = cmd.split()[-1]
+        if var in calcd:
+            mem[key] = ~mem[var]
+            nextStep(key)
+
+    if "LSHIFT" in cmd.split():
+        var1 = cmd.split()[0]
+        var2 = cmd.split()[2]
+        key = cmd.split()[-1]
+        if var1 in mem:
+            var1 = int(mem[var1])
+            var2 = int(var2)
+            mem[key] = var1 << var2
+            nextStep(key)
+
+    if "RSHIFT" in cmd.split():
+        var1 = cmd.split()[0]
+        var2 = cmd.split()[2]
+        key = cmd.split()[-1]
+        if var1 in mem:
+            var1 = int(mem[var1])
+            var2 = int(var2)
+            mem[key] = var1 >> var2
+            nextStep(key)
+
+    if "AND" in cmd.split():
+        var1 = cmd.split()[0]
+        var2 = cmd.split()[2]
+        key = cmd.split()[-1]
+        if var1 in calcd and var2 in calcd:
+            mem[key] = mem[var1] & mem[var2]
+            nextStep(key)
+
+
+def nextStep(root):
+    for i in lines:
+        if root in i.split('->')[0].split():
+            print(i)
+            ops(i)
+
+
+findRoots()
+calcd = roots
+
+
+def driver():
+    for i in roots:
+        nextStep(i)
+
+
+driver()
+print(mem)
